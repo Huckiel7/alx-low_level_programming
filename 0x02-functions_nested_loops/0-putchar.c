e347daec4309ec5d6987f1f253c2c9b61f8a22b5
Huckiel <huckielsukai7@gmail.com>
2022-06-07 03:39:50 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
+ * main - prints entry point
  *
- * Return: nothing, return void
+ * Return: Always 0.
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
