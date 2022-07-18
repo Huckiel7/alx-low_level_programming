b230bd10b2dfa1fdabff6a53893900a6e9c34b70
Huckiel <huckielsukai7@gmail.com>
2022-07-18 10:19:33 -0700

    task 12, Print (safe version) redone

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,31 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * print_listint_safe - prints the elements in linked list
+ * @h: head of listint_t type
  *
- * Return: number of nodes in the list
+ * Return: size_t, number of nodes. Exits with 98 on failure
  */
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint_safe(const listint_t *h)
 {
-    size_t num = 0;
-    long int diff;
+	size_t n_nodes = 0;
+	const listint_t *temp_h = NULL, *loop_node= NULL;
 
-    while (head)
-    {
-        diff = head - head->next;
-        num++;
-        printf("[%p] %d\n", (void *)head, head->n);
-        if (diff > 0)
-            head = head->next;
-        else
-        {
-            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-            break;
-        }
-    }
-
-    return (num);
+	temp_h = h;
+	if (!h)
+		exit(98);
+	while (temp_h)
+	{
+		if (h->next == temp_h && n_nodes > 2)
+		{
+			loop_node = temp_h;
+			printf("loop node: [%p] %d\n", (void *)loop_node, loop_node->n);
+			exit(98);
+		}
+		printf("[%p] %d\n", (void *)temp_h, temp_h->n);
+		temp_h = temp_h->next;
+		n_nodes++;
+	}
+	return (n_nodes);
 }
